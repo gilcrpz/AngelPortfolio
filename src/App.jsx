4673754f5e0557dfb700a24b25e5d78f0f25dece
angel <cayabyabangel6363@gmail.com>
2026-09-05 20:44:51 +0800

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ export default function App() {
   }, [darkMode]);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/AngelPortfolio">
       <ScrollToHash />
       <ScrollReveal />
       <div className="container">
